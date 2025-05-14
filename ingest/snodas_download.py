f3c1b0346e19e38f5a358c59e11bfefacfef8aa4
Andrew Nelson <anelson2037@gmail.com>
2025-05-14 13:55:33 -0700

    extract downloaded .tar

--- ingest/snodas_download.py
+++ ingest/snodas_download.py
@@ -1,11 +1,12 @@
 import os
 import requests
+import tarfile
 from datetime import datetime
 from pathlib import Path
 
 # ========== Configuration ==========
 
-OUTPUT_DIR = "data"
+BASE_DIR = "data"
 BASE_URL = "https://noaadata.apps.nsidc.org/NOAA/G02158/unmasked"
 DEFAULT_DATE = datetime.today()
 
@@ -18,12 +19,28 @@ def build_snodas_url(date: datetime) -> str:
     filename = f"SNODAS_unmasked_{file_date_str}.tar"
     return f"{BASE_URL}/{year}/{month_str}/{filename}", filename
 
+def build_output_path(date: datetime) -> str:
+    """
+    Constructs the output path for the downloaded file based on the date.
+    """
+    year = date.strftime("%Y")
+    month_str = date.strftime("%m_%b")  # e.g., "05_May"
+    file_date_str = date.strftime("%Y%m%d")
+    filename = f"SNODAS_unmasked_{file_date_str}.tar"
+    return os.path.join(BASE_DIR, year, month_str)
+
+
 def download_snodas_file(date: datetime):
     """
     Downloads the SNODAS .tar file for a given date and saves it to OUTPUT_DIR.
     """
     url, filename = build_snodas_url(date)
-    local_path = Path(OUTPUT_DIR) / filename
+    local_path = build_output_path(date)
+    output_dir = os.path.join(local_path, filename)
+
+    print(f"url: {url}")
+    print(f"filename: {filename}")
+    print(f"local path: {local_path}")
 
     print(f"Downloading SNODAS file for {date.strftime('%Y-%m-%d')}...")
     print(f"URL: {url}")
@@ -32,13 +49,22 @@ def download_snodas_file(date: datetime):
         response = requests.get(url, stream=True)
         response.raise_for_status()
 
-        os.makedirs(OUTPUT_DIR, exist_ok=True)
+        os.makedirs(local_path, exist_ok=True)
 
-        with open(local_path, "wb") as f:
+        with open(output_dir, "wb") as f:
             for chunk in response.iter_content(chunk_size=8192):
                 f.write(chunk)
 
-        print(f"Saved to {local_path}")
+        print(f"Saved to {output_dir}")
+
+        with tarfile.open(output_dir, "r") as tar:
+            tar.extractall(path=local_path)
+        print(f"Extracted {filename} to {local_path}")
+
+        os.remove(output_dir)
+        print(f"Removed tar file {output_dir}")
+
+
 
     except requests.exceptions.RequestException as e:
         print(f"Download failed: {e}")
